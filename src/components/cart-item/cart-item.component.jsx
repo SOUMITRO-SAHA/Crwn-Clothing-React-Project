fbd3a565a769d838bfc3eeccfafb9c5144616f17
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2022-09-02 10:21:34 +0530

    added in add_to_cart feature

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,12 +1,18 @@
 import "./cart-item.style.scss";
 
 const CartItem = ({ cartItem }) => {
-  const { name, quantity } = cartItem;
+  const { name, imageUrl, price, quantity } = cartItem;
 
   return (
-    <div>
-      <h2>{name}</h2>
-      <span>{quantity}</span>
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          Price : {quantity} X ${price}
+        </span>
+        <span className="price">Quantity : {quantity}</span>
+      </div>
     </div>
   );
 };
